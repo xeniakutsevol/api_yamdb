07197401227d5a5e0c016368919781ecc804052c
Илья Алферов <iliya.alferov2015@yandex.ru>
2022-05-09 17:45:48 +0300

    urls

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from rest_framework import routers
+from .views import CategoriesViewSet, GenresViewSet, TitlesViewSet
+
+app_name = 'api'
+
+v1_router = routers.DefaultRouter()
+v1_router.register(r'categories', CategoriesViewSet, basename='categories')
+v1_router.register(r'genres', GenresViewSet, basename='genres')
+v1_router.register(r'titles', TitlesViewSet, basename='titles')
+
+
+urlpatterns = [
+    path('v1/', include(v1_router.urls)),
+]
