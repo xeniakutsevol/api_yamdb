1ddf1033821a57c4be714b4920fab0ae7dc2a077
Артем Черепан <artemii.nikolaevi4@gmail.com>
2022-05-15 11:46:28 +0300

    feature: view, serializer, permission

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,6 +1,31 @@
 from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS
 
 
 class UserAdminPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.role == 'admin'
+
+
+class ReviewCommentPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS or request.user
+            and request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        if request.user.is_authenticated and request.method in [
+            'PATCH',
+            'DELETE'
+        ]:
+            return (
+                request.user.is_admin
+                or request.user.is_moderator
+                or obj.author == request.user
+            )
+        if request.user.is_authenticated:
+            return True
